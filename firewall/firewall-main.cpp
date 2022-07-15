270bffb0b21af8cc63c2801fa007baffe30bb305
Tima32 <4timonomit4@gmail.com>
2022-07-15 18:22:59 +0300

    Test aprs arguments.

--- firewall/firewall-main.cpp
+++ firewall/firewall-main.cpp
@@ -3,16 +3,47 @@
 
 using namespace std;
 
+struct Felter
+{
+	uint8_t proto;
+
+	uint32_t ip_src;
+	uint32_t ip_dst;
+	
+	uint16_t port_src;
+	uint16_t port_dst;
+};
+
+bool AddFilter(const size_t pos, const ArgumentParser& ap)
+{
+	try
+	{
+		auto& proto = ap.at(pos + 1);
+
+		cout << proto << endl;
+
+		cout << "--------" << endl;
+	}
+	catch (const std::exception& e)
+	{
+		cerr << e.what() << endl;
+		return false;
+	}
+
+	return true;
+}
 int main(int argc, const char** argv)
 {
 	ArgumentParser ap(argc, argv);
 
 	try
 	{
-		for (size_t pos = 0; pos != -1 && pos < ap.size(); pos++)
+		for (size_t pos = 0; pos < ap.size(); pos++)
 		{
 			pos = ap.find("-f", pos);
+			if (pos == -1) break;
 			cout << ap[pos] << " " << pos << endl;
+			AddFilter(pos, ap);
 		}		
 	}
 	catch(const std::exception& e)
