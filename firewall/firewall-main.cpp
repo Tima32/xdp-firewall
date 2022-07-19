07ce8f00cbc5a8b4ef952daf46163fc037ff80e7
Tima32 <4timonomit4@gmail.com>
2022-07-19 13:56:50 +0300

    Add help.

--- firewall/firewall-main.cpp
+++ firewall/firewall-main.cpp
@@ -14,6 +14,18 @@ struct Felter
 	uint16_t port_dst;
 };
 
+void PrintHelp()
+{
+	static constexpr auto str = 
+	"Usage: xdp-firewall [options]\n"
+	"	--block [protocol]\n"
+	"	--filter [protocol] [ip-src] [ip-dst] [port-src] [port-dst]\n"
+	"	--range [protocol] [ip-src-begin] [ip-src-end] [ip-dst-begin] [ip-dst-end]\n"
+	"		[port-src-begin] [port-src-end] [port-dst-begin] [port-dst-end]\n"
+	;
+
+	cout << str << endl;
+}
 bool AddFilter(const size_t pos, const ArgumentParser& ap)
 {
 	try
@@ -36,6 +48,13 @@ int main(int argc, const char** argv)
 {
 	ArgumentParser ap(argc, argv);
 
+	auto help_pos = ap.find("--help");
+	if (help_pos != ArgumentParser::not_found || argc == 1)
+	{
+		PrintHelp();
+		return 0;
+	}
+
 	try
 	{
 		for (size_t pos = 0; pos < ap.size(); pos++)
@@ -52,7 +71,5 @@ int main(int argc, const char** argv)
 		exit(-1);
 	}
 	
-
-	cout << "Hello world" << endl;
 	return 0;
 }
